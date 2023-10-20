927b63a1c4231df44cb0a2d59ef7fe8290cde05e
Naner Nunes <naner@naner.com.br>
2023-10-19 23:44:28 -0300

    Adding www due blocked by CORS policy

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: 'https://kyse.com.br/',
+    base: 'https://www.kyse.com.br/',
     plugins: [react()],
     resolve: {
         alias: {
